6451463e75e5df4c0c110f1c691d4ca64d5f39cf
QuentinSu <quentin.sutkowski@etu.utc.fr>
2019-01-30 17:01:24 +0000

    Add entities server, serverReminder

---
+++ api/src/AppBundle/Entity/Server.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ServerRepository")
+ */
+class Server
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Project")
+     */
+    private $project;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $liveDate;
+
+    /**
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\ServerReminder", mappedBy="server", orphanRemoval=true)
+     */
+    private $serverReminders;
+
+    public function __construct()
+    {
+        $this->serverReminders = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getProject(): ?Project
+    {
+        return $this->project;
+    }
+
+    public function setProject(?Project $project): self
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    public function getLiveDate(): ?\DateTimeInterface
+    {
+        return $this->liveDate;
+    }
+
+    public function setLiveDate(?\DateTimeInterface $liveDate): self
+    {
+        $this->liveDate = $liveDate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|ServerReminder[]
+     */
+    public function getServerReminders(): Collection
+    {
+        return $this->serverReminders;
+    }
+
+    public function addServerReminder(ServerReminder $serverReminder): self
+    {
+        if (!$this->serverReminders->contains($serverReminder)) {
+            $this->serverReminders[] = $serverReminder;
+            $serverReminder->setServer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeServerReminder(ServerReminder $serverReminder): self
+    {
+        if ($this->serverReminders->contains($serverReminder)) {
+            $this->serverReminders->removeElement($serverReminder);
+            // set the owning side to null (unless already changed)
+            if ($serverReminder->getServer() === $this) {
+                $serverReminder->setServer(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ api/src/AppBundle/Entity/ServerReminder.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ServerReminderRepository")
+ */
+class ServerReminder
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Server", inversedBy="serverReminders")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $server;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=1000, nullable=true)
+     */
+    private $comment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getServer(): ?Server
+    {
+        return $this->server;
+    }
+
+    public function setServer(?Server $server): self
+    {
+        $this->server = $server;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
